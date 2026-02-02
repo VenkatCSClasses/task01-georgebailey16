72e06d84c9964a6820c00996f8dc2792dd7b4164
georgebailey16 <123581511+georgebailey16@users.noreply.github.com>
2026-02-01 21:27:28 -0500

    withdrawTest() tests written --Tests failed--

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -17,9 +17,22 @@ class BankAccountTest {
     void withdrawTest() throws InsufficientFundsException{
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
+        assertEquals(100, bankAccount.getBalance(), 0.001); //class: valid withdraw
+        BankAccount bankAccount2 = new BankAccount("c@d.com", 200);
+        bankAccount2.withdraw(0.01);
+        assertEquals(199.99, bankAccount2.getBalance(), 0.001); //class: lower boundary withdraw
+        BankAccount bankAccount3 = new BankAccount("e@f.com", 200);
+        bankAccount3.withdraw(199.99);
+        assertEquals(0.01, bankAccount3.getBalance(), 0.001); //class: upper boundary withdraw
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300)); //class: overdraw -- above upper boundary
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(-.01)); //class: negative withdraw -- below lower boundary
+        BankAccount bankAccount4 = new BankAccount("g@h.com", 100);
+        assertThrows(InsufficientFundsException.class, () -> bankAccount4.withdraw(0.0000000000000001)); //class: invalid small withdraw -- lower invalid boundary
+        assertThrows(InsufficientFundsException.class, () -> bankAccount4.withdraw(0.001)); //class: invalid small withdraw -- upper invalid boundary
+        assertThrows(InsufficientFundsException.class, () -> bankAccount4.withdraw(0)); //class: non-negative invalid withdraw -- lower invalid boundary
+        assertThrows(InsufficientFundsException.class, () -> bankAccount4.withdraw(100.01)); //class: non-negative invalid withdraw -- upper invalid boundary
 
-        assertEquals(100, bankAccount.getBalance(), 0.001);
-        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300));
+
     }
 
     @Test
